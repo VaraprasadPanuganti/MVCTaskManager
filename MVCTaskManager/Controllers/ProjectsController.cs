665541a262072caf11599c9d6f24e3af7484004c
Varaprasad Panuganti <panugnativaraprasad169@gmail.com>
2024-10-02 22:40:31 +0530

    CRUD Operations completed

--- MVCTaskManager/Controllers/ProjectsController.cs
+++ MVCTaskManager/Controllers/ProjectsController.cs
@@ -9,21 +9,21 @@ namespace MVCTaskManager.Controllers
     {
         private readonly IConfiguration _configuration;
         private readonly ILogger<ProjectsController> _logger;
+        private readonly string connectionString;
         public ProjectsController(IConfiguration configuration, ILogger<ProjectsController> logger)
         {
             _configuration = configuration;
             _logger = logger;
+            connectionString = _configuration.GetConnectionString("DefaultConnection"); ;
         }
 
         [HttpGet]
         [Route("/")]
-        [Route("api/Projects")]
+        [Route("api/getProjects")]
         public async Task<List<Project>> GetProjects()
         {
             try
             {
-                _logger.LogInformation("GetProjects Entered");
-                string connectionString = _configuration.GetConnectionString("DefaultConnection");
                 DataSet dsProjects = new DataSet();
                 using (SqlConnection con = new SqlConnection(connectionString))
                 {
@@ -36,16 +36,140 @@ namespace MVCTaskManager.Controllers
                     return projects;
                 }
             }
-            catch(Exception ex) 
+            catch (Exception ex)
             {
-                _logger.LogError(ex.Message,"GetProjects Error Message");
-                _logger.LogError(ex.StackTrace,"GetProjects Error StackTrace");
+                _logger.LogError(ex.Message, "GetProjects Error Message");
+                _logger.LogError(ex.StackTrace, "GetProjects Error StackTrace");
                  return new List<Project>();
             }
 
         }
+
+        [HttpPost]
+        [Route("api/insertProject")]
+        public async Task<IActionResult> InsertProject([FromBody] Project project)
+        {
+            try
+            {
+                if (project.ProjectId <= 0 || string.IsNullOrWhiteSpace(project.ProjectName) || project.TeamSize <= 0 || project.DateOfStart == DateTime.MinValue)
+                {
+                    return BadRequest("One of the Project parameter is null or empty.");
+                }
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                {
+                    await sqlConnection.OpenAsync(); 
+                    SqlCommand sqlCommand = new SqlCommand("InsertProject", sqlConnection);
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.Add(new SqlParameter("@ProjectId", project.ProjectId));
+                    sqlCommand.Parameters.Add(new SqlParameter("@ProjectName", project.ProjectName));
+                    sqlCommand.Parameters.Add(new SqlParameter("@DateOfStart", project.DateOfStart));
+                    sqlCommand.Parameters.Add(new SqlParameter("@TeamSize", project.TeamSize));
+                    await sqlCommand.ExecuteNonQueryAsync();
+                }
+                 return StatusCode(201,new { message = "Successfully inserted." });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, "InsertProject Error Message");
+                _logger.LogError(ex.StackTrace, "InsertProject Error Stacktrace");
+                 return StatusCode(500, new { message = "Internal server error while inserting project." });
+            }
+        }
+
+        [HttpPost]
+        [Route("api/updateProject")]
+        public async Task<IActionResult> UpdateProject([FromBody] Project project)
+        {
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                {
+                    await sqlConnection.OpenAsync();
+                    SqlCommand sqlCommand = new SqlCommand("UpdateProject", sqlConnection);
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.Add(new SqlParameter("@ProjectId", project.ProjectId));
+                    sqlCommand.Parameters.Add(new SqlParameter("@ProjectName", project.ProjectName));
+                    sqlCommand.Parameters.Add(new SqlParameter("@DateOfStart", project.DateOfStart));
+                    sqlCommand.Parameters.Add(new SqlParameter("@TeamSize", project.TeamSize));
+
+                    SqlParameter outputParam = new SqlParameter("@Result", SqlDbType.Int)
+                    {
+                        Direction = ParameterDirection.Output
+                    };
+                    sqlCommand.Parameters.Add(outputParam);
+                    await sqlCommand.ExecuteNonQueryAsync();
+
+                    // Get the value of the output parameter
+                    int result = (int)outputParam.Value;
+                    if (result == 1)
+                    {
+                        return StatusCode(200, new { message = "project Updated Successfully." });
+                    }
+                    else 
+                    {
+                        return StatusCode(404, new { message = "record not found in db" });
+                    }
+                }              
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, "UpdateProject Error Message");
+                _logger.LogError(ex.StackTrace, "UpdateProject Error SatckTrace");
+                return StatusCode(500, new { message = "Internal server error while updating project request." });
+
+            }
+        }
+
+        [HttpGet]
+        [Route("api/deleteProject/{projectId}")]
+        public async Task<IActionResult> deleteProject(int projectId)
+        {
+            try
+            {
+                if (projectId <= 0)
+                {
+                    return BadRequest("Invalid Project ID.");
+                }
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                {
+                    await sqlConnection.OpenAsync();
+                    SqlCommand sqlCommand = new SqlCommand("deleteProject", sqlConnection);
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.Add(new SqlParameter("@projectId", projectId));
+                    SqlParameter outputParam = new SqlParameter("@Result", SqlDbType.Int)
+                    {
+                        Direction=ParameterDirection.Output
+                    };
+                    sqlCommand.Parameters.Add(outputParam);
+                    await sqlCommand.ExecuteNonQueryAsync();
+
+                    //Get the value of output param
+                    int result = (int)outputParam.Value;
+                    if (result == 1)
+                    {
+                        return StatusCode(200, new { message = "project Updated Successfully." });
+                    }
+                    else if (result == 0)
+                    {
+                        return StatusCode(404, new { message = "no record found in db." });
+                    }
+                    else
+                    {
+                        return StatusCode(500, new { message = "Internal server error while updating project request." });
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, "deleteProject Error Message");
+                _logger.LogError(ex.StackTrace, "deleteProject Error stackTrace");
+                return StatusCode(500, new { message = "Internal server error while updating project request." });
+            }
+        }
         // Method to convert DataTable to List<T>
-        public  async Task<List<T>> ConvertDataTableToList<T>(DataTable table) where T : new()
+        public async Task<List<T>> ConvertDataTableToList<T>(DataTable table) where T : new()
         {
             try
             {
--- MVCTaskManager/Models/Project.cs
+++ MVCTaskManager/Models/Project.cs
@@ -6,7 +6,7 @@ namespace MVCTaskManager.Models
     {
         [Key]        
         public int ProjectId { get; set; }
-        public string? PorjectName { get; set; }
+        public string? ProjectName { get; set; }
         public DateTime DateOfStart { get; set; }
         public int  TeamSize { get; set; }
     }
